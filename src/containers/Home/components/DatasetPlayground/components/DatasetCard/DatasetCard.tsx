4210186f7408558ff7ab9a545e8da3afd22e0eb1
Hector Angel Gomez Robaina <hectorangel2001@gmail.com>
2023-08-23 13:41:16 -0400

    Update Dataset card

--- src/containers/Home/components/DatasetPlayground/components/DatasetCard/DatasetCard.tsx
+++ src/containers/Home/components/DatasetPlayground/components/DatasetCard/DatasetCard.tsx
@@ -1,14 +1,21 @@
+import { Key } from "@modules/app/modules/icon/components"
+
 export default function DatasetCard() {
   return (
-    <div className='bg-darkColor flex flex-col w-[400px] rounded text-white'>
-      <header className='flex font-fontBold justify-center py-5 px-10 text-3xl border-b-[1px] border-white'>
-        User
+    <div className='bg-darkColor flex flex-col w-[400px] rounded text-white stroke-white'>
+      <header className='flex justify-center py-5 px-10 border-b-[1px] border-white items-center gap-x-3'>
+        <h1 className='text-3xl font-fontBold'>User</h1>
+        <p className='text-2xl'>(50)</p>
       </header>
 
       <div className='flex flex-col py-2'>
         {["id", "username", "password"].map((field, i) => (
           <div key={i} className='flex text-lg items-center py-2 px-10 justify-between'>
-            <p className='font-fontMedium'>{field}</p>
+            <div className='flex items-center gap-x-3'>
+              <Key size={20} />
+              <p className='font-fontMedium'>{field}</p>
+            </div>
+
             <p className='font-fontMedium'>id.uuid</p>
           </div>
         ))}
