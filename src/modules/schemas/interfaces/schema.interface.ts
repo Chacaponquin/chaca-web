99bcef5787272a6e91da3a04cb3d7178c642d87c
Hector Angel Gomez Robaina <hectorangel2001@gmail.com>
2023-08-29 14:07:51 -0400

    Add showName property un Schema and SchemaOption

--- src/modules/schemas/interfaces/schema.interface.ts
+++ src/modules/schemas/interfaces/schema.interface.ts
@@ -4,13 +4,12 @@ export interface Schema {
   name: string
   options: SubOption[]
   id: string
+  showName: string
 }
 
 export interface SubOption {
   id: string
   name: string
   arguments: Argument[]
-  exampleValue: unknown
-  description: string
-  route: string
+  showName: string
 }
