5d30d34917b0353bd2679f2cef0a16425c295c3d
Hector Angel Gomez Robaina <hectorangel2001@gmail.com>
2023-09-04 15:29:39 -0400

    Add more test for contact us page

--- cypress/e2e/create-user-message/create-user-message.cy.ts
+++ cypress/e2e/create-user-message/create-user-message.cy.ts
@@ -15,4 +15,31 @@ describe("# Create user message e2e test", () => {
 
     cy.get("#message-success-modal").should("exist")
   })
+
+  it("Create message with empty email. Should show a toast error", () => {
+    cy.get("input[name=email]").clear()
+    cy.get("textarea[name=message]").clear().type("test message")
+    cy.get("input[name=title]").clear().type("test title")
+    cy.get("button[type=submit]").click()
+
+    cy.get("#toast-error").should("exist")
+  })
+
+  it("Create message with empty title. Should show a toast error", () => {
+    cy.get("input[name=email]").clear().type("test@gmail.com")
+    cy.get("textarea[name=message]").clear().type("test message")
+    cy.get("input[name=title]").clear()
+    cy.get("button[type=submit]").click()
+
+    cy.get("#toast-error").should("exist")
+  })
+
+  it("Create message with empty message. Should show a toast error", () => {
+    cy.get("input[name=email]").clear().type("test@gmail.com")
+    cy.get("textarea[name=message]").clear()
+    cy.get("input[name=title]").clear().type("test title")
+    cy.get("button[type=submit]").click()
+
+    cy.get("#toast-error").should("exist")
+  })
 })
