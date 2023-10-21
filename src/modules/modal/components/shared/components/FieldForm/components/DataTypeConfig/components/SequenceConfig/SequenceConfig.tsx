26c1107bd3679dfc71167d6fd9fef7a36368de8a
Hector Angel Gomez Robaina <hectorangel2001@gmail.com>
2023-10-20 20:50:27 -0400

    Fix input numbers in sequence field form

--- src/modules/modal/components/shared/components/FieldForm/components/DataTypeConfig/components/SequenceConfig/SequenceConfig.tsx
+++ src/modules/modal/components/shared/components/FieldForm/components/DataTypeConfig/components/SequenceConfig/SequenceConfig.tsx
@@ -2,26 +2,28 @@ import { ChacaNumberInput } from "@form/components"
 import { FormInputSection } from "@modules/modal/components/shared/shared/components"
 import { useId } from "react"
 
+interface Props {
+  startsWith: number
+  step: number
+  handleChangeSequenceStartsWith: (v: number) => void
+  handleChangeSequenceStep: (v: number) => void
+}
+
 export default function SequenceConfig({
   startsWith,
   step,
   handleChangeSequenceStartsWith,
   handleChangeSequenceStep,
-}: {
-  startsWith: number
-  step: number
-  handleChangeSequenceStartsWith: (v: number) => void
-  handleChangeSequenceStep: (v: number) => void
-}) {
+}: Props) {
   const startId = useId()
   const stepId = useId()
 
   return (
-    <div className="flex items-center justify-between gap-3">
+    <div className="flex items-center justify-between gap-3 w-full">
       <FormInputSection labelText={"Starts with"} id={startId}>
         <ChacaNumberInput
           value={startsWith}
-          size={140}
+          size={"full"}
           dimension="large"
           onChange={handleChangeSequenceStartsWith}
           min={0.1}
@@ -32,7 +34,7 @@ export default function SequenceConfig({
       <FormInputSection id={stepId} labelText={"Step"}>
         <ChacaNumberInput
           value={step}
-          size={140}
+          size={"full"}
           dimension="large"
           onChange={handleChangeSequenceStep}
           min={0.1}
