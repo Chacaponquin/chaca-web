7b9c1ad9c46c30ebecd52321c9c8c834fd15bce0
Hector Angel Gomez Robaina <hectorangel2001@gmail.com>
2023-10-24 14:16:44 -0400

    Update routes

--- src/modules/app/modules/socket/constants/SOCKET_EVENTS.ts
+++ src/modules/app/modules/socket/constants/SOCKET_EVENTS.ts
@@ -1,8 +1,5 @@
 export enum SOCKET_EVENTS {
-  CREATE_DATASETS = "createDatasets",
-  GET_FILE_URL = "fileUrl",
-  CREATION_ERROR = "creationError",
-  CONNECT = "connect",
-  DISCONNECT = "disconnect",
-  CONNECT_ERROR = "connect_error",
+  CREATE_DATASETS = "create-datasets",
+  GET_FILE_URL = "get-file-url",
+  CREATION_ERROR = "creation-error",
 }
