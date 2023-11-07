dbaa73eb8d75e88bdf0809cb35fd1a3c9482c255
Hector Angel Gomez Robaina <hectorangel2001@gmail.com>
2023-11-07 10:40:31 -0500

    Add copy code in landing

--- src/containers/LandigPage/components/Presentation/components/Content/components/Buttons/components/CodeLink/CodeLink.tsx
+++ src/containers/LandigPage/components/Presentation/components/Content/components/Buttons/components/CodeLink/CodeLink.tsx
@@ -1,7 +1,15 @@
 import { ArrowRight, Copy } from "@modules/app/modules/icon/components"
+import { useToast } from "@modules/app/modules/toast/hooks"
 
 export default function CodeLink() {
+  const { toastSuccess } = useToast()
+
   const CODE = "npm install chaca -dev"
+
+  function handleCopy() {
+    toastSuccess("Copied to clipoard")
+    navigator.clipboard.writeText(CODE)
+  }
 
   return (
     <div className="px-6 py-2 border-[1px] border-scale-10 bg-scale-5 stroke-white fill-white rounded flex justify-center items-center">
@@ -9,7 +17,7 @@ export default function CodeLink() {
 
       <p className="font-fontCode ml-3 mr-7">{CODE}</p>
 
-      <button>
+      <button className="transition-all duration-200 hover:scale-105" onClick={handleCopy}>
         <Copy size={22} />
       </button>
     </div>
