68027a78d082926b1699c62f32e0a1622f9ddba8
Hector Angel Gomez Robaina <hectorangel2001@gmail.com>
2023-11-15 08:48:17 -0500

    Update welcome message

--- src/containers/Home/components/NoDatasetsMessage/NoDatasetsMessage.tsx
+++ src/containers/Home/components/NoDatasetsMessage/NoDatasetsMessage.tsx
@@ -8,15 +8,19 @@ interface Props {
 }
 
 export default function NoDatasetsMessage({ handleCreateDataset }: Props) {
-  const { MESSAGE } = useLanguage({ MESSAGE: { en: "Create Dataset", es: "Crear Dataset" } })
+  const { MESSAGE, WELCOME_MESSAGE } = useLanguage({
+    MESSAGE: { en: "Create Dataset", es: "Crear Dataset" },
+    WELCOME_MESSAGE: {
+      en: "Create your first dataset and see all the actions you can perform with its fields",
+      es: "Crea tu primer dataset y mira todas las acciones que puedes realizar con sus campos",
+    },
+  })
 
   return (
     <section className="bg-white dark:bg-scale-4 w-full flex flex-grow flex-col pt-24 items-center px-20 esm:px-8">
       <Image image={APP_IMAGES.WELCOME} className="object-contain w-[460px]" />
 
-      <p className="text-xl mt-8 text-black dark:text-white text-center">
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consectetur, officia.
-      </p>
+      <p className="text-xl mt-8 text-black dark:text-white text-center">{WELCOME_MESSAGE}</p>
 
       <ChacaSimpleButton
         color="secondary"
