0044119186133ae145143636e6bd703fd6b13c58
Héctor Angel Gomez <hectorangel2001@gmail.com>
2024-02-04 09:40:44 -0500

    Update footer text

--- src/containers/LandigPage/components/Footer/components/Left/Left.tsx
+++ src/containers/LandigPage/components/Footer/components/Left/Left.tsx
@@ -1,6 +1,6 @@
 export default function Left() {
   return (
-    <p className="text-scale-11 text-xl">
+    <p className="text-scale-11 text-xl esm:text-center">
       Copyright © {new Date().getFullYear()} Chaca - MIT License
     </p>
   )
