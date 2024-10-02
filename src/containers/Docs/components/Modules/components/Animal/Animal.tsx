25c8a410888220f332160edd9c6d2e050c76b434
Héctor Angel Gomez <hectorangel2001@gmail.com>
2024-10-02 14:44:21 -0400

    feat: Add animal module docs

--- src/containers/Docs/components/Modules/components/Animal/Animal.tsx
+++ src/containers/Docs/components/Modules/components/Animal/Animal.tsx
@@ -1,3 +1,66 @@
+import { MethodSection } from "../../shared/components"
+import {
+  BEAR_CODE,
+  BIRD_CODE,
+  CAT_CODE,
+  CETACEAN_CODE,
+  COW_CODE,
+  CROCODILIA_CODE,
+  DOG_CODE,
+  FISH_CODE,
+  HORSE_CODE,
+  INSECT_CODE,
+  LION_CODE,
+  RABBIT_CODE,
+  RODENT_CODE,
+  TYPE_CODE,
+  SNAKE_CODE,
+} from "./domain"
+
 export default function Animal() {
-  return <></>
+  return (
+    <>
+      <MethodSection apiId="dog" method="dog" params={[]} title="Dog" code={DOG_CODE} />
+
+      <MethodSection apiId="bear" method="bear" code={BEAR_CODE} title="Bear" params={[]} />
+
+      <MethodSection apiId="bird" code={BIRD_CODE} method="bird" params={[]} title="Bird" />
+
+      <MethodSection apiId="cat" title="Cat" code={CAT_CODE} method="cat" params={[]} />
+
+      <MethodSection
+        apiId="cetacean"
+        title="Cetacean"
+        code={CETACEAN_CODE}
+        method="cetacean"
+        params={[]}
+      />
+
+      <MethodSection apiId="cow" title="Cow" code={COW_CODE} method="cow" params={[]} />
+
+      <MethodSection
+        apiId="crocodilia"
+        code={CROCODILIA_CODE}
+        method="crocodilia"
+        params={[]}
+        title="Crocodilia"
+      />
+
+      <MethodSection apiId="fish" code={FISH_CODE} method="fish" params={[]} title="Fish" />
+
+      <MethodSection apiId="horse" code={HORSE_CODE} method="horse" params={[]} title="Horse" />
+
+      <MethodSection apiId="insect" code={INSECT_CODE} method="insect" params={[]} title="Insect" />
+
+      <MethodSection apiId="lion" code={LION_CODE} method="lion" params={[]} title="Lion" />
+
+      <MethodSection apiId="rabbit" code={RABBIT_CODE} method="rabbit" params={[]} title="Rabbit" />
+
+      <MethodSection apiId="rodent" code={RODENT_CODE} method="rodent" params={[]} title="Rodent" />
+
+      <MethodSection apiId="snake" code={SNAKE_CODE} method="snake" params={[]} title="Snake" />
+
+      <MethodSection apiId="type" code={TYPE_CODE} method="type" params={[]} title="Animal type" />
+    </>
+  )
 }
---
+++ src/containers/Docs/components/Modules/components/Animal/domain/index.ts
@@ -0,0 +1,59 @@
+export const DOG_CODE = `
+modules.animal.dog() // 'Irish Water Spaniel'
+`
+
+export const BEAR_CODE = `
+modules.animal.bear() // 'Singapuria'
+`
+
+export const BIRD_CODE = `
+modules.animal.bird() // 'Singapuria'
+`
+
+export const CAT_CODE = `
+modules.animal.cat() // 'Bengal'
+`
+
+export const CETACEAN_CODE = `
+modules.animal.cetacean() // 'Spinner Dolphin'
+`
+
+export const COW_CODE = `
+modules.animal.cow() // 'Brava'
+`
+
+export const CROCODILIA_CODE = `
+modules.animal.crocodilia() // 'Philippine Crocodile'
+`
+
+export const FISH_CODE = `
+modules.animal.fish() // 'Mandarin fish'
+`
+
+export const HORSE_CODE = `
+modules.animal.horse() // 'Swedish Warmblood'
+`
+
+export const INSECT_CODE = `
+modules.animal.insect() // 'Pyramid ant'
+`
+
+export const LION_CODE = `
+modules.animal.lion() // 'Northeast Congo Lion'
+`
+
+export const RODENT_CODE = `
+modules.animal.rodent() // 'Cuscomys ashanika'
+`
+
+export const RABBIT_CODE = `
+modules.animal.rabbit() // 'Florida White'
+`
+
+export const TYPE_CODE = `
+modules.animal.type() // 'Singapuria'
+`
+
+export const SNAKE_CODE = `
+modules.animal.snake() // 'Eyelash viper'
+`
