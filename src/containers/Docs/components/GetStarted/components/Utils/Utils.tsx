8a9e180930b5b65e8fc8dc1e2804fbb14a2f098d
Héctor Angel Gomez <hectorangel2001@gmail.com>
2024-10-07 17:03:59 -0400

    fix: Add utils docs header text

--- src/containers/Docs/components/GetStarted/components/Utils/Utils.tsx
+++ src/containers/Docs/components/GetStarted/components/Utils/Utils.tsx
@@ -1,3 +1,4 @@
+import { Link, MiniCode, P } from "@modules/shared/modules/markdown/components/Markdown/components"
 import {
   CamelCase,
   Capitalize,
@@ -6,10 +7,17 @@ import {
   ReplaceSymbols,
   SumDateRange,
 } from "./components"
+import { OVERVIEW } from "@modules/docs/domain/core/sections/modules"
 
 export default function Utils() {
   return (
     <>
+      <P>
+        Chaca provee algunas de las functiones que son utilizadas para generar los valores en los{" "}
+        <Link to={OVERVIEW.redirect}>módulos</Link>. Para utilizarlas solo es necesario acceder a la
+        propiedad <MiniCode>chaca.utils</MiniCode> donde se encuentran los siguientes métodos.
+      </P>
+
       <CamelCase />
       <Capitalize />
       <CapitalizeWord />
