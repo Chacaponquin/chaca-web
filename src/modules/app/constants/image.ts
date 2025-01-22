ba536202acdc858d52a10663172888f24c0beea9
Chacaponquin <hectorangel2001@gmail.com>
2025-01-22 11:23:08 -0500

    feat: remove unnecesary images

--- src/modules/app/constants/image.ts
+++ src/modules/app/constants/image.ts
@@ -1,12 +1,10 @@
 export const APP_IMAGES = {
   LOGO: { image: "/logo/chaca-logo.png", alt: "chaca-logo" },
   EMPTY_FIELDS: { image: "/images/Bored.svg", alt: "empty-fields-image" },
-  WAIT_REQUEST: { image: "/images/wait_request.png", alt: "wait_request_image" },
   EMPTY: { image: "/images/empty.png", alt: "empty-image" },
   ERROR: { image: "/images/error.png", alt: "error-image" },
   CONTACT: { image: "/images/contact.png", alt: "contact-image" },
   SUCCESS: { image: "/images/modal.png", alt: "success-image" },
-  SIGN_UP_IMAGE: { image: "/images/signUp-image.png", alt: "sign-up-image" },
   WELCOME: { image: "/images/welcome.png", alt: "chaca-welcome-image" },
   PRESENTATION: {
     SIMPLE_DATASET: { image: "/images/simple-schema.png", alt: "simple-schema" },
